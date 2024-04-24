ef86133532ca42f76480675d01e9bae5a58ddfe2
2615283 <131881245+NightLocke15@users.noreply.github.com>
2024-04-24 10:58:50 +0200

    Changes to folders

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,8 @@
 const root = "/WSOA3028A_2615283"
 
 const menuItems = [
-    {name: "Home", href: `${root}/index.html`},
-    {name: "Portfolio", href: `${root}/portfolio/portfolio.html`},
+    {name: "Home", href: root + `/index.html`},
+    {name: "Portfolio", href: root  + `/portfolio/portfolio.html`},
     {name: "Profile", href: `${root}/profile/profile.html`},
     {name: "Designs", href: `${root}/designs/design.html`},
     {name: "Blogs", href: `${root}/blogs/blogs.html`},
