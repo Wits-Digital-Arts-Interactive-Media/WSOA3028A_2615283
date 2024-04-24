8dfd7e0873ebefcc6d673015a98edea5edf07c96
2615283 <131881245+NightLocke15@users.noreply.github.com>
2024-04-24 11:10:13 +0200

    JS Fixes More

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,28 +1,26 @@
 const root = "/WSOA3028A_2615283"
 
 const menuItems = [
-    {name: "Home", href: root + `/index.html`},
-    {name: "Portfolio", href: root  + `/portfolio/portfolio.html`},
-    {name: "Profile", href: `${root}/profile/profile.html`},
-    {name: "Designs", href: `${root}/designs/design.html`},
-    {name: "Blogs", href: `${root}/blogs/blogs.html`},
-    {name: "Essays", href: `${root}/essays/essays.html`},
+    {name: "Home", href: `./profile/index.html`},
+    {name: "Portfolio", href: `./profile/index.html`},
+    {name: "Profile", href: `./profile/index.html`},
+    {name: "Designs", href: `./designs/index.html`},
+    {name: "Blogs", href: `./blogs/index.html`},
+    {name: "Essays", href: `./essays/index.html`},
 ]
 
 export function Initialize(currentPage) {
-    const nav = document.querySelector ("header > nav")
-    const ul = document.createElement ("ul")
+    const nav = document.querySelector("header > nav")
+    const ul = document.createElement("ul")
     for (let menuItem of menuItems) {
-        const li = document.createElement ("li")
+        const li = document.createElement("li")
         if (currentPage != menuItem.name) {
             const a = document.createElement ("a")
             a.innerText = menuItem.name
             a.setAttribute("href", menuItem.href)
             li.appendChild(a)
         }
-        else if (currentPage == menuItem.name){
-            li.innerText = menuItem.name
-        }       
+        else (li.innerText = menuItem.name)       
         ul.appendChild(li)        
     }
     nav.appendChild(ul)    
