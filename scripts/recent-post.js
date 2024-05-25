710867063ad01c7123ad1f02772ed60db5ad9b57
2615283 <131881245+NightLocke15@users.noreply.github.com>
2024-05-25 15:39:16 +0200

    CSS part 7

--- scripts/recent-post.js
+++ scripts/recent-post.js
@@ -1,15 +1,15 @@
 const root =  "/WSOA3028A_2615283/"
 
 const blogs = [
-    {name: "Blog-Post-1", href: `${root}blogs/blog1.html`},
-    {name: "Blog-Post-2", href: `${root}blogs/blog2.html`},
-    {name: "Blog-Post-3", href: `${root}blogs/blog3.html`},
-    {name: "Blog-Post-4", href: `${root}blogs/blog4.html`},
-    {name: "Blog-Post-5", href: `${root}blogs/blog5.html`},
-    {name: "Blog-Post-6", href: `${root}blogs/blog6.html`},
-    {name: "Blog-Post-7", href: `${root}blogs/blog7.html`},
-    {name: "Blog-Post-8", href: `${root}blogs/blog8.html`},
-    {name: "Blog-Post-9", href: `${root}blogs/blog9.html`},
+    {name: "Blog Post 1", href: `${root}blogs/blog1.html`},
+    {name: "Blog Post 2", href: `${root}blogs/blog2.html`},
+    {name: "Blog Post 3", href: `${root}blogs/blog3.html`},
+    {name: "Blog Post 4", href: `${root}blogs/blog4.html`},
+    {name: "Blog Post 5", href: `${root}blogs/blog5.html`},
+    {name: "Blog Post 6", href: `${root}blogs/blog6.html`},
+    {name: "Blog Post 7", href: `${root}blogs/blog7.html`},
+    {name: "Blog Post 8", href: `${root}blogs/blog8.html`},
+    {name: "Blog Post 9", href: `${root}blogs/blog9.html`},
 ]
 
 export function recentPosts() {
